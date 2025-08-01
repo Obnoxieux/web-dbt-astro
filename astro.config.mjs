2ad701a2690efe38f8a65708a76be12e8b633a97
David Battefeld <davidbattefeld@gmail.com>
2025-08-01 09:34:45 +0200

    chore!: remove env setup from Astro config
    Type-safe Astro environment unfortunately only really work with variables that are available statically at build time. For my docker setup I need dynamic variables provided by `getSecret()`.

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,27 +17,6 @@ export default defineConfig({
 
   integrations: [svelte( { extensions: ['.svelte']})],
 
-  env: {
-    schema: {
-      // API access
-      BSM_API_KEY: envField.string({context: "server", access: "secret"}),
-      GITHUB_ACCESS_TOKEN: envField.string({context: "server", access: "secret"}),
-      
-      // SMTP configuration
-      SMTP_SERVER: envField.string({context: "server", access: "secret"}),
-      SMTP_PORT: envField.string({context: "server", access: "secret"}),
-      SMTP_USER: envField.string({context: "server", access: "secret", optional: true}),
-      SMTP_PASS: envField.string({context: "server", access: "secret", optional: true}),
-      SMTP_MAIL_TO_ADDRESS: envField.string({context: "server", access: "secret"}),
-      SMTP_MAIL_FROM_ADDRESS: envField.string({context: "server", access: "secret"}),
-
-      // Public contact information
-      PUBLIC_CONTACT_EMAIL_ADDRESS: envField.string({context: "client", access: "public"}),
-      PUBLIC_CONTACT_PHONE: envField.string({context: "client", access: "public"}),
-      PUBLIC_CONTACT_ADDRESS: envField.string({context: "client", access: "public"})
-    }
-  },
-
   adapter: node({
     mode: "standalone"
   })
