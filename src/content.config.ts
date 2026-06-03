f87957550294461680078a753b53f123307a5f73
David Battefeld <post@davidbattefeld.de>
2026-06-03 09:39:38 +0200

    feat: define more content collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,47 @@
-import { defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
-import { z } from 'astro/zod';
+import { defineCollection, reference } from "astro:content";
+import { file, glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const blog = defineCollection({
-  loader: glob({pattern: '**/*.md', base: './blog'}),
+  loader: glob({ pattern: "**/*.md", base: "./blog" }),
   schema: z.object({
     title: z.string(),
     author: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
-  })
+  }),
 });
 
-export const collections = { blog };
+const languages = defineCollection({
+  loader: file("./src/data/languages.toml"),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      sample: z.string().optional(),
+      icon: image(),
+    }),
+});
+
+const frameworks = defineCollection({
+  loader: file("./src/data/frameworks.toml"),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      description: z.string().optional(),
+      icon: image(),
+    }),
+});
+
+const projects = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/data/projects" }),
+  schema: z.object({
+    name: z.string(),
+    description: z.string(),
+    repositoryURL: z.string(),
+    languages: z.array(reference("languages")),
+    frameworks: z.array(reference("frameworks")),
+  }),
+});
+
+export const collections = { blog, languages, frameworks, projects };
